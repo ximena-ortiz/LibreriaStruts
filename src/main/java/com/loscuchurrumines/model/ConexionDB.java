ba4888a6d00f6b751aa8d170b6029618d5b6a36d
ximena-ortiz <xo2021071080@virtual.upt.pe>
2023-10-22 19:46:17 -0500

    listo

--- src/main/java/com/loscuchurrumines/model/ConexionDB.java
+++ src/main/java/com/loscuchurrumines/model/ConexionDB.java
@@ -5,11 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionDB {
-    private static final String URL_STRING= "jdbc:mysql://127.0.0.1:3306/dbtienda";
-    private static final String USER = "root";
+    private static final String URL_STRING = "jdbc:postgresql://ep-small-haze-88087949.us-east-2.aws.neon.tech/dbtienda";
+    private static final String USER = "ximena-ortiz";
     private static final String PASS = "";
 
-    public static Connection getConnection() throws SQLException{
+    public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL_STRING, USER, PASS);
+        
     }
 }
+
+
